4f828678a28a78a2ae172842f6c1529de55a0700
Ed C <ed.ch@colemanzoo.com>
2023-05-22 13:51:14 -0600

    classify, fix escape (self.!)

--- banner.py
+++ banner.py
@@ -1,96 +1,99 @@
  # https://stackoverflow.com/questions/53638972/displaying-an-image-full-screen-in-python
 
-import sys, os
+import os
 
 import tkinter
-# PIL == pillow
 from PIL import Image, ImageTk
 import time
 
 logit = False
 
-defaultDir = "C:\\FSC\\banner\\default_photos\\"
-picDir = "C:\\Users\\rfsc\\Pictures\\"
-imageTypes = [".jpg", ".png"] # assume three-letter file types (MS Windows)
-delay = 5
-keepCycling = True
-win = None
-canvas = None
-width = None
-height = None
+class Banner:
 
-def init():
-    global win, canvas, width, height
-    win = tkinter.Tk()
-    width = win.winfo_screenwidth() # get the display size
-    height = win.winfo_screenheight() # get the display size
-    win.overrideredirect(1) # no title bar
-    win.geometry("%dx%d+0+0" % (width, height)) # create a window that covers the screen
-    win.focus_set() # give it the focus
-    win.attributes("-topmost", 1) # and to be extra careful, force it to the top
-    win.bind("<Escape>", lambda e: endCycling(e))
+    defaultDir = "C:\\FSC\\banner\\default_photos\\" # default photo(s) in case picDir is empty
+    picDir = "C:\\Users\\rfsc\\Pictures\\" # where to find pictures to show (in alpha order)
+    imageTypes = [".jpg", ".png", "jpeg"] # assume three-letter file types (MS Windows)
+    delay = 5 # number of seconds to show each pic
+    keepCycling = True # change to False to stop the program
+    win = None # main Tk window
+    canvas = None # Tk canvas for drawing the pics
+    width = None # screen width in pixels
+    height = None # screen height in pixels
 
-    # canvas is our workarea in the window
-    canvas = tkinter.Canvas(win,width=width,height=height)
-    canvas.pack()
-    canvas.configure(background='black')
+    def __init__(self):
+        #global win, canvas, width, height
+        self.win = tkinter.Tk()
+        self.width = self.win.winfo_screenwidth() # get the display width
+        self.height =self.win.winfo_screenheight() # get the display height
+        self.win.overrideredirect(1) # don't show the title bar
+        self.win.geometry("%dx%d+0+0" % (self.width, self.height)) # create a window that covers the screen
+        self.win.focus_set() # give it the focus
+        self.win.attributes("-topmost", 1) # and to be extra careful, force it to the top
+        #self.win.bind("<Escape>", lambda e: self.endCycling(e)) # escape key stops the program
 
-def quit():
-    win.destroy()
+        # canvas is our workarea in the window
+        self.canvas = tkinter.Canvas(self.win,width=self.width,height=self.height,border=0)
+        self.canvas.pack()
+        self.canvas.configure(background='black')
 
-def showPIL(pilImage):
-    global win, canvas, width, height
-    imgWidth, imgHeight = pilImage.size
-    # resize photo to full screen 
-    ratio = min(width/imgWidth, height/imgHeight)
-    imgWidth = int(imgWidth*ratio)
-    imgHeight = int(imgHeight*ratio)
-    pilImage = pilImage.resize((imgWidth,imgHeight), Image.LANCZOS)   
-    image = ImageTk.PhotoImage(pilImage)
-    imagesprite = canvas.create_image(width/2,height/2,image=image)
-    win.update_idletasks()
-    win.update()
-    #root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
-    #win.bind("<Escape>", lambda e: endCycling(e))
+        while(self.keepCycling):
+            if logit: print("\nNext pass:")
+            self.cycle()
+
+        quit()
 
 
-def cycle():
-    global win, canvas, width, height
-    sourceDir = picDir
-    try:
-        fileNames = os.listdir(sourceDir)
-    except:
-        fileNames = []
-    # if no files in the target directory, use the defaults
-    if(fileNames == []):
-        sourceDir = defaultDir
-        fileNames = os.listdir(sourceDir)
-    if logit: print(fileNames)
-    for fileName in fileNames:
-        path = sourceDir + fileName
-        if logit: print("showing:", path)
-        if path[-4:] in imageTypes:
-            try:
-                image=Image.open(path)
-                foo = delay
-                while((foo > 0) and (keepCycling)):
-                    foo -= 1
-                    if logit: print("foo, keepCycling:", foo, keepCycling)
-                    showPIL(image)
-                    time.sleep(1)
+    def quit(self):
+        self.win.destroy()
 
-            except:
-                if logit: print(f"{path} failed")
+    def showPIL(self, pilImage):
+        #global win, canvas, width, height
+        imgWidth, imgHeight = pilImage.size
+        # resize photo to full screen 
+        ratio = min(self.width/imgWidth, self.height/imgHeight)
+        imgWidth = int(imgWidth*ratio)
+        imgHeight = int(imgHeight*ratio)
+        pilImage = pilImage.resize((imgWidth,imgHeight), Image.LANCZOS)   
+        image = ImageTk.PhotoImage(pilImage)
+        imagesprite = self.canvas.create_image(self.width/2,self.height/2,image=image)
+        self.win.update_idletasks()
+        self.win.update()
+        self.win.bind("<Escape>", lambda e: self.endCycling(e)) # escape key stops the program
+        #root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
 
-def endCycling(e):
-    global keepCycling
+    def cycle(self):
+        #global win, canvas, width, height
+        sourceDir = self.picDir
+        try:
+            fileNames = os.listdir(sourceDir)
+        except:
+            fileNames = []
+        # if no files in the target directory, use the defaults
+        if(fileNames == []):
+            sourceDir = self.defaultDir
+            fileNames = os.listdir(sourceDir)
+        if logit: print(fileNames)
+        for fileName in fileNames:
+            path = sourceDir + fileName
+            if logit: print("showing:", path)
+            if path[-4:] in self.imageTypes:
+                try:
+                    image=Image.open(path)
+                    foo = self.delay
+                    while((foo > 0) and (self.keepCycling)):
+                        foo -= 1
+                        if logit: print("foo, keepCycling:", foo, self.keepCycling)
+                        self.showPIL(image)
+                        time.sleep(1)
 
-    keepCycling = False
-    if logit: print("stopping:", e)
+                except:
+                    if logit: print(f"{path} failed")
+
+    def endCycling(self, e):
+        global keepCycling
+
+        self.keepCycling = False
+        if logit: print("stopping:", e)
 
 if __name__ == '__main__':            
-    init()
-    while(keepCycling):
-        if logit: print("\nNext pass:")
-        cycle()
-    quit()
+    banner = Banner()
